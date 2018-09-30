b65321460bdbcaa5b736b31a305dd77d9828290d
Hardy <hardiansyah.iiI@gmail.com>
2018-09-30 23:08:47 +0700

    refactor views to use generic class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,38 +1,20 @@
-from rest_framework import generics, mixins
+from rest_framework import generics
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericApiView):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets or create a new snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericApiView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update, or delete a snippet instance.
     """
     queryset = Snippet.objects.all()
     serializer = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.delete(request, *args, **kwargs)
