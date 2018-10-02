2910af501bd696171d439b81aa3c765faf20491b
Hardy <hardiansyah.iiI@gmail.com>
2018-10-02 17:06:31 +0700

    add names to urlpatterns

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,8 +3,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^snippets/$', views.SnippetList.as_view(), name="snippets_get_post"),
+    url(
+        r'^snippets/(?P<pk>[0-9]+)/$',
+        views.SnippetDetail.as_view(),
+        name="snippets_get_put_delete"
+    ),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
