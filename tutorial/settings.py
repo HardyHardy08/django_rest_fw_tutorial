e0c57d6f8f534dd549927cf614bd70ac2a05df94
Hardy <hardiansyah.iiI@gmail.com>
2018-10-04 00:16:55 +0700

    update requirements.txt to include psycopg2 and edit setings to include relevant hosts

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = '&p=@=(3rq1t=yp8-upn)=t4v9gc09_7polra&pz0qwr+u6k9*^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['snippetsapi.com']
 
 
 # Application definition
